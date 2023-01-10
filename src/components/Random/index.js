1a6cb8f03f799667e7dc0eed1598f0011d0e6f93
harshithdhara291 <harshithdhara291@gmail.com>
2023-01-10 06:36:18 +0000

    final commit

--- src/components/Random/index.js
+++ src/components/Random/index.js
@@ -23,9 +23,8 @@ class Random extends Component {
   state = {
     isLoading: true,
     quotes: {
-      content:
-        'Knowledge comes, but wisdom lingers. It may not be difficult to store up in the mind a vast quantity of facts within a comparatively short time, but the ability to form judgments requires the severe discipline of hard work and the tempering heat of experience and maturity.',
-      author: 'Calvin Coolidge',
+      content: '',
+      author: '',
     },
     tags: '',
   }
