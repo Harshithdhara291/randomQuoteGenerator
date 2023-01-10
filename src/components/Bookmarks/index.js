534fa51c9a4b3bf482fb3353c95d87a2d7d86d67
harshithdhara291 <harshithdhara291@gmail.com>
2023-01-10 07:01:41 +0000

    final commit

--- src/components/Bookmarks/index.js
+++ src/components/Bookmarks/index.js
@@ -7,28 +7,56 @@ const Bookmarks = () => (
   <Context.Consumer>
     {value => {
       const {bookmarksList} = value
+      const isEmpty = bookmarksList.length === 0
       console.log(bookmarksList)
       return (
         <>
-          <div className="main-containers">
-            <div className="linkss">
-              <Link to="/">
-                <button type="button" className="home-buttons">
-                  Home
-                </button>
-              </Link>
-              <Link to="/bookmarks">
-                <button type="button" className="bookmarks-buttons">
-                  Bookmarks
-                </button>
-              </Link>
+          {isEmpty ? (
+            <div className="main-containers">
+              <div className="linkss">
+                <Link to="/">
+                  <button type="button" className="home-buttons">
+                    Home
+                  </button>
+                </Link>
+                <Link to="/bookmarks">
+                  <button type="button" className="bookmarks-buttons">
+                    Bookmarks
+                  </button>
+                </Link>
+              </div>
+              <h1 className="empty">No Bookmarks!!</h1>
+              <p className="empty-para">
+                Click{' '}
+                <Link to="/">
+                  <button type="button" className="link">
+                    here
+                  </button>
+                </Link>{' '}
+                to add{' '}
+              </p>
             </div>
-            <ul>
-              {bookmarksList.map(each => (
-                <BookmarkItem each={each} key={each.quotes.id} />
-              ))}
-            </ul>
-          </div>
+          ) : (
+            <div className="main-containers">
+              <div className="linkss">
+                <Link to="/">
+                  <button type="button" className="home-buttons">
+                    Home
+                  </button>
+                </Link>
+                <Link to="/bookmarks">
+                  <button type="button" className="bookmarks-buttons">
+                    Bookmarks
+                  </button>
+                </Link>
+              </div>
+              <ul>
+                {bookmarksList.map(each => (
+                  <BookmarkItem each={each} key={each.quotes.id} />
+                ))}
+              </ul>
+            </div>
+          )}
         </>
       )
     }}
